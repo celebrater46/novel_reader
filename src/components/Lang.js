29a1898d7dc98db34b1cf10a17f33c6388fc4f92
hideru <jota.16.stigma.world@gmail.com>
2021-05-13 19:21:06 +0900

    add multi languages sentences.

--- src/components/Lang.js
+++ src/components/Lang.js
@@ -1,30 +1,22 @@
-import React, { useMemo } from 'react';
-import Link from "next/link";
 import consoleLog from "../static/consoleLog";
+import Img from "../static/Img";
+
+const getClasses = (lang) => {
+    if(lang === 0) {
+        return ["lang", "lang void"];
+    } else {
+        return ["lang void", "lang"];
+    }
+}
 
 const Lang = (props) => {
     consoleLog([props.lang], "props.lang", "Lang", "components/Lang");
+    const classNames = getClasses(props.lang);
 
     return (
         <div className="lang">
-            <Link key="jp" href={{ pathname: '/', query: { page: props.pageNames[props.pageNum][0], lang: 1 } }}>
-                {(()=> {
-                    if(props.lang == 1) {
-                        return <img src="../static/img/jpn.png" className="lang" />;
-                    } else {
-                        return <img src="../static/img/jpn.png" className="lang void" />;
-                    }
-                })()}
-            </Link>
-            <Link key="en" href={{ pathname: '/', query: { page: props.pageNames[props.pageNum][0], lang: 0 } }}>
-                {(()=> {
-                    if(props.lang == 0) {
-                        return <img src="../static/img/usa.png" className="lang" />;
-                    } else {
-                        return <img src="../static/img/usa.png" className="lang void" />;
-                    }
-                })()}
-            </Link>
+            <Img fname="jpn.png" imgClass={classNames[0]} isLocal={false} />
+            <Img fname="usa.png" imgClass={classNames[1]} isLocal={false} />
         </div>
     );
 }
